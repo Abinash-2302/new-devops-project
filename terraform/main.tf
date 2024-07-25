3cb844766caea86eb468a64b5eedef53c17b2666
Abinash-2302 <abinashs721sahoo@gmail.com>
2024-07-25 21:10:24 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -103,7 +103,7 @@ resource "aws_instance" "frontend" {
     connection {
       type        = "ssh"
       user        = "ec2-user"  # Update as necessary
-      private_key = file("//wsl.localhost/Ubuntu/home/abianshsahoo_123\MyKeyPair1.pem")  # Update with your key file path
+      private_key = file("//wsl.localhost/Ubuntu/home/abianshsahoo_123/MyKeyPair1.pem")  # Update with your key file path
       host        = self.public_ip
     }
   }

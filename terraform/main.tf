22d237e4fd1ba9f96d47fe81d1b15c4574ebf7d7
Abinash-2302 <abinashs721sahoo@gmail.com>
2024-07-30 20:36:21 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -113,13 +113,13 @@ resource "aws_instance" "frontend" {
   provisioner "remote-exec" {
     heredoc =[
       "mkdir tmp",
-      "eval "$(ssh-agent -s)",
-      "ssh-add /home/abianshsahoo_123/MyKeyPair1.pem",
-      "chmod 600 /home/abianshsahoo_123/MyKeyPair1.pem",
-      "User ubuntu"
-      "IdentityFile /home/abianshsahoo_123/MyKeyPair1.pem"
-      "ForwardAgent yes"
-      "ssh -i /home/abianshsahoo_123/MyKeyPair1.pem ubuntu@aws_instance.backend.private_ip",
+      "eval "$(ssh-agent -s)" \
+      "ssh-add /home/abianshsahoo_123/MyKeyPair1.pem" \
+      "chmod 600 /home/abianshsahoo_123/MyKeyPair1.pem" \
+      "User ubuntu" \ 
+      "IdentityFile /home/abianshsahoo_123/MyKeyPair1.pem" \
+      "ForwardAgent yes" \
+      "ssh -i /home/abianshsahoo_123/MyKeyPair1.pem ubuntu@aws_instance.backend.private_ip" \
       "echo Hello, World! > /tmp/hello.txt"
     ]
     connection {

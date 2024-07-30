db7f494a747180ba33912a1bf62cb5929a2cea75
Abinash-2302 <abinashs721sahoo@gmail.com>
2024-07-30 19:00:47 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,7 +62,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["106.222.188.149/32"]
+    cidr_blocks = ["13.233.177.0/29"]
   }
 
   egress {

823e2ae11f67f7d18786b13dd87a926db2a0bd8c
Abinash-2302 <abinashs721sahoo@gmail.com>
2024-08-01 21:50:17 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,6 +29,7 @@ resource "aws_subnet" "private" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.private_subnet_cidr
   availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
   
   tags = {
     Name = "private-subnet"

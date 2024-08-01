b54e4bd52d3da302380e19a8806b5f879447b630
Abinash-2302 <abinashs721sahoo@gmail.com>
2024-08-01 11:53:55 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,7 +51,7 @@ resource "aws_route_table" "public" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = "10.0.0.0/16"
     gateway_id = aws_internet_gateway.igw.id
   }
 }

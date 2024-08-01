826c3f2af051765f5bb19beaaa366f7dc740d901
Abinash-2302 <abinashs721sahoo@gmail.com>
2024-08-02 00:09:25 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -92,6 +92,7 @@ resource "aws_security_group" "backend" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+   security_groups = [aws_security_group.app.id]
   }
 
   ingress {

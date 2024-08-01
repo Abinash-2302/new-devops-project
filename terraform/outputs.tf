cbc952ce803f608107bb72bae715f3d81705410f
Abinash-2302 <abinashs721sahoo@gmail.com>
2024-08-01 23:25:23 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "frontend_public_ip" {
 output "backend_private_ip" {
   value = aws_instance.backend.private_ip
 }
+
+output "backend_public_ip" {
+  value = aws_instance.backend.public_ip
+}

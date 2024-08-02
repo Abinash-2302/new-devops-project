602fc79f88351ef2bb44242ff4f1a0f0ed4d7d51
Abinash-2302 <abinashs721sahoo@gmail.com>
2024-08-03 03:05:47 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -124,6 +124,12 @@ resource "aws_instance" "frontend" {
   provisioner file{
  source = "/home/abianshsahoo_123/2-tier-app/frontend.sh"
  destination = "/home/ubuntu/frontend.sh"
+ provisioner "remote-exec" {
+    inline = [
+       "chmod +x /home/ubuntu/backend.sh",
+      "/home/ubuntu/backend.sh",
+      "echo 'File uploaded successfully!'"
+    ]
   connection {
       type        = "ssh"
       user        = "ubuntu"  # Update as necessary
@@ -131,12 +137,10 @@ resource "aws_instance" "frontend" {
       host        = self.public_ip
     }
 }
+}
     provisioner "remote-exec" {
     inline = [
       "mkdir tmp",
-      "chmod +x /home/ubuntu/frontend.sh",
-      "/home/ubuntu/frontend.sh",
-      "echo 'File uploaded successfully!'"
     ]
     connection {
       type        = "ssh"
@@ -161,21 +165,26 @@ resource "aws_instance" "backend" {
  provisioner file{
  source = "/home/abianshsahoo_123/2-tier-app/backend.sh"
  destination = "/home/ubuntu/backend.sh"
+ provisioner "remote-exec" {
+    inline = [
+    
+      "chmod +x /home/ubuntu/backend.sh",
+      "/home/ubuntu/backend.sh",
+      "echo 'File uploaded successfully!'"
+    ]
  connection {
       type        = "ssh"
       user        = "ubuntu"  # Update as necessary
       private_key = file("/home/abianshsahoo_123/MyKeyPair1.pem")  # Update with your key file path
       host        = self.public_ip
     }
-
+}
   }
   
  provisioner "remote-exec" {
     inline = [
       "mkdir tmp",
-      "chmod +x /home/ubuntu/backend.sh",
-      "/home/ubuntu/backend.sh",
-      "echo 'File uploaded successfully!'"
+      
     ]
     connection {
       type        = "ssh"

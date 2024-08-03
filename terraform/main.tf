8ff62745763db56471cace271e2f70a1f115f952
Abinash-2302 <abinashs721sahoo@gmail.com>
2024-08-03 13:28:19 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -125,7 +125,7 @@ resource "aws_instance" "frontend" {
   provisioner file{
  source = "/home/abianshsahoo_123/2-tier-app/frontend.sh"
  destination = "/home/ubuntu/frontend.sh"
-}
+
  provisioner "remote-exec" {
     inline = [
        "chmod +x /home/ubuntu/backend.sh",
@@ -138,7 +138,7 @@ resource "aws_instance" "frontend" {
       private_key = file("/home/abianshsahoo_123/MyKeyPair1.pem")  # Update with your key file path
       host        = self.public_ip
     }
-
+}
 }
     
 
@@ -172,7 +172,7 @@ resource "aws_instance" "backend" {
  provisioner file{
  source = "/home/abianshsahoo_123/2-tier-app/backend.sh"
  destination = "/home/ubuntu/backend.sh"
-}
+
  provisioner "remote-exec" {
     inline = [
     
@@ -186,7 +186,7 @@ resource "aws_instance" "backend" {
       private_key = file("/home/abianshsahoo_123/MyKeyPair1.pem")  # Update with your key file path
       host        = self.public_ip
     }
-
+}
   }
   
 
